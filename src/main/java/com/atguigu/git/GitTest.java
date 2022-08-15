0709ecbaabbc5fb4d3540ea4795b3d039739c5ff
lijing <346744457@qq.com>
2022-08-15 22:31:44 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好git");
         System.out.println("你好git2");
+        System.out.println("你好git3");
     }
 }
